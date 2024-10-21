c71355f50ef5e3caabb8c67f9e18dcd7b693a96a
BogdanVeligorskyi <bogdan.veligorskyi@gmail.com>
2024-10-21 18:46:47 +0300

    Added 2 of 4 components in digital inputs modes

--- src/App.js
+++ src/App.js
@@ -32,13 +32,134 @@ function App() {
             <div class="col digital-inputs">
               <h2>Digital Inputs</h2>
 
-              <div class="digital-input-types">
+              <table>
+                <tr>
+                  <th></th>
+                  <th>1</th>
+                  <th>2</th>
+                  <th>3</th>
+                  <th>4</th>
+                </tr>
+                <tr>
+                  <td>Button</td>
+                  <td>
+                  &nbsp;&nbsp;<input type="radio" id="buttonCh1" name="di_channel_1" value=""/>
+                  <label for="buttonCh1"></label>
+                  </td>
+                  <td>&nbsp;&nbsp;
+                  <input type="radio" id="buttonCh2" name="di_channel_2" value=""/>
+                <label for="buttonCh2"></label>
+                  </td>
+                  <td>&nbsp;&nbsp;
+                  <input type="radio" id="buttonCh3" name="di_channel_3" value=""/>
+                <label for="buttonCh3"></label>
+                  </td>
+                  <td>&nbsp;&nbsp;
+                  <input type="radio" id="buttonCh4" name="di_channel_4" value=""/>
+                <label for="buttonCh4"></label>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Switch</td>
+                  <td>
+                  &nbsp;&nbsp;<input type="radio" id="switchCh1" name="di_channel_1" value=""/>
+                  <label for="switchCh1"></label>
+                  </td>
+                  <td>&nbsp;&nbsp;
+                  <input type="radio" id="switchCh2" name="di_channel_2" value=""/>
+                <label for="switchCh2"></label>
+                  </td>
+                  <td>&nbsp;&nbsp;
+                  <input type="radio" id="switchCh3" name="di_channel_3" value=""/>
+                <label for="switchCh3"></label>
+                  </td>
+                  <td>&nbsp;&nbsp;
+                  <input type="radio" id="switchCh4" name="di_channel_4" value=""/>
+                <label for="switchCh4"></label>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Gen</td>
+                  <td>
+                  &nbsp;&nbsp;<input type="radio" id="genCh1" name="di_channel_1" value=""/>
+                  <label for="genCh1"></label>
+                  </td>
+                  <td>&nbsp;&nbsp;
+                  <input type="radio" id="genCh2" name="di_channel_2" value=""/>
+                <label for="genCh2"></label>
+                  </td>
+                  <td>&nbsp;&nbsp;
+                  <input type="radio" id="genCh3" name="di_channel_3" value=""/>
+                <label for="genCh3"></label>
+                  </td>
+                  <td>&nbsp;&nbsp;
+                  <input type="radio" id="genCh4" name="di_channel_4" value=""/>
+                <label for="genCh4"></label>
+                  </td>
+                </tr>
+                <tr>
+                  <td>None</td>
+                  <td>
+                  &nbsp;&nbsp;<input type="radio" id="noneCh1" name="di_channel_1" value=""/>
+                  <label for="noneCh1"></label>
+                  </td>
+                  <td>&nbsp;&nbsp;
+                  <input type="radio" id="noneCh2" name="di_channel_2" value=""/>
+                <label for="noneCh2"></label>
+                  </td>
+                  <td>&nbsp;&nbsp;
+                  <input type="radio" id="noneCh3" name="di_channel_3" value=""/>
+                <label for="noneCh3"></label>
+                  </td>
+                  <td>&nbsp;&nbsp;
+                  <input type="radio" id="noneCh4" name="di_channel_4" value=""/>
+                <label for="noneCh4"></label>
+                  </td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td>
+                    <div class="digital-input-wrapper">
+                      <button class="digital-input-button"></button>
+                    </div>
+                  </td>
+                  <td>
+                  on
+                    <div class="digital-input-wrapper">
+                    
+                      <label class="switch-vertical">
+                        <input type="checkbox"/>
+                        <span class="slider-vertical"></span>
+                      </label>
+                      
+                    </div>
+                    off
+                  </td>
+                  
+                  <td>
+                    <div class="digital-input-wrapper">
+                    <button class="digital-input-switch"></button>
+
+                    </div>
+                  </td>
+                  <td>
+                    <div class="digital-input-wrapper">
+                      <button class="digital-input-button"></button>
+                    </div>
+                  </td>
+                </tr>
+              </table>
+              
+            </div>
+
+              {/* <div class="digital-input-types">
                 Button  <br/>
                 Switch  <br/>
                 Gen  <br/>
                 None  <br/>
+                <br/>
+                <br/>
               </div>
-
              
               <div class="digital-input-ch1">
                 <fieldset>
@@ -52,7 +173,11 @@ function App() {
                   <label for="genCh1"></label><br/>
                   <input type="radio" id="noneCh1" name="di_channel_1" value=""/>
                   <label for="noneCh1"></label><br/>
+                  
                 </fieldset>
+                <div class="digital-input-wrapper">
+                  <button class="digital-input-button"></button>    
+                </div>
               </div>
              
               <div class="digital-input-ch2">
@@ -68,6 +193,9 @@ function App() {
                 <input type="radio" id="noneCh2" name="di_channel_2" value=""/>
                 <label for="noneCh2"></label><br/>
                 </fieldset>
+                <div class="digital-input-wrapper">
+                  <button class="digital-input-switch"></button>    
+                </div>
               </div>
               <div class="digital-input-ch3">
               <fieldset>
@@ -77,10 +205,11 @@ function App() {
                 <label for="buttonCh3"></label><br/>
                 <input type="radio" id="switchCh3" name="di_channel_3" value=""/>
                 <label for="switchCh3"></label><br/>
-                <input type="radio" id="genCh3" name="di_channel_2" value=""/>
+                <input type="radio" id="genCh3" name="di_channel_3" value=""/>
                 <label for="genCh3"></label><br/>
                 <input type="radio" id="noneCh3" name="di_channel_3" value=""/>
                 <label for="noneCh3"></label><br/>
+                <button></button>
                 </fieldset>
               </div>
               <div class="digital-input-ch4">
@@ -95,9 +224,10 @@ function App() {
                 <label for="genCh4"></label><br/>
                 <input type="radio" id="noneCh4" name="di_channel_4" value=""/>
                 <label for="noneCh4"></label><br/>
+                <button></button>
                 </fieldset>
               </div>
-            </div>
+            </div> */}
 
             <div class="col camera-view">
               <h2>Camera View From Lab</h2>
